2904e2d14f55f06e066b6ee8ff75c6ca9872d77c
jk007x219 <143647058+jk007x219@users.noreply.github.com>
2026-09-18 02:22:25 +0700

    feat: add activity status categories to staff page

--- src/app/staff/activities/page.tsx
+++ src/app/staff/activities/page.tsx
@@ -1,12 +1,16 @@
 import StaffActivitiesPage from "@/components/staff/StaffActivitiesPage";
 import ActivityWorkflowPanel from "@/components/staff/ActivityWorkflowPanel";
+import StaffActivityCategoryTabs from "@/components/staff/StaffActivityCategoryTabs";
 import styles from "./page.module.css";
 
 export default function StaffActivitiesRoutePage() {
   return (
     <>
-      <div className="staff-activity-workflow-root">
-        <ActivityWorkflowPanel />
+      <div className="px-4 pt-4 sm:px-6 lg:px-7">
+        <StaffActivityCategoryTabs />
+        <div className="staff-activity-workflow-root">
+          <ActivityWorkflowPanel />
+        </div>
       </div>
       <div className={styles.legacyActivityPage}>
         <StaffActivitiesPage />
