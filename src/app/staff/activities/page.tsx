c98f2fbfc132b87c44ad5b51638c4dbe9ed5e74e
jk007x219 <143647058+jk007x219@users.noreply.github.com>
2026-09-18 02:02:49 +0700

    Connect staff activity management to staged workflow controls

--- src/app/staff/activities/page.tsx
+++ src/app/staff/activities/page.tsx
@@ -1,5 +1,13 @@
 import StaffActivitiesPage from "@/components/staff/StaffActivitiesPage";
+import ActivityWorkflowPanel from "@/components/staff/ActivityWorkflowPanel";
 
 export default function StaffActivitiesRoutePage() {
-  return <StaffActivitiesPage />;
+  return (
+    <>
+      <div className="px-4 pt-4 sm:px-6 lg:px-7">
+        <ActivityWorkflowPanel />
+      </div>
+      <StaffActivitiesPage />
+    </>
+  );
 }
