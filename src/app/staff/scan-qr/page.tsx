3a5a979849e378b4ef15ec244e47ca97582dd6a6
jk007x219 <143647058+jk007x219@users.noreply.github.com>
2026-09-23 00:31:04 +0700

    Make existing QR Scanner page usable without staff shell

--- src/app/staff/scan-qr/page.tsx
+++ src/app/staff/scan-qr/page.tsx
@@ -1,6 +1,5 @@
 import ScanQrPage from "@/components/scan/ScanQrPage";
-import StaffShell from "@/components/staff/StaffShell";
 
 export default function StaffScanQrRoute() {
-  return <ScanQrPage Shell={StaffShell} activePath="/staff/scan-qr" />;
+  return <ScanQrPage />;
 }
